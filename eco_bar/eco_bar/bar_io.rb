ebc5d51b8c2770263cf5c3d4c93c04126e1e63fc
Rob Zwissler <rob@zwissler.org>
2016-08-22 16:25:42 -0700

    Added render() and SFMono support

--- eco_bar/eco_bar/bar_io.rb
+++ eco_bar/eco_bar/bar_io.rb
@@ -1,4 +1,9 @@
 module EcoBar
+
+  require 'fileutils'
+
+  ECOBEE_URL = 'https://www.ecobee.com/consumerportal/index.html'
+
   class BarIO
     attr_accessor :index
     attr_reader :max_index, :thermostats
@@ -11,14 +16,41 @@ module EcoBar
       @token = token
       @thermostats = thermostats || load_thermostats
       @index = [index, max_index].min
+
       @base_command = %Q{bash="#{$0}" refresh=true terminal=false}
+
+      #check_for_sfmono
     end
 
     def about
-      puts "ecoBar v#{EcoBar::VERSION} | #{color(:dark)} href=#{EcoBar::HOMEPAGE}"
-      auto_update = EcoBar::AutoUpdate.new
-      unless auto_update.up_to_date?
-        puts "Update Available | #{color(:hot)} #{@base_command}  param1=update"
+      render("ecoBar v#{EcoBar::VERSION}", color: :dark, href: EcoBar::HOMEPAGE)
+
+      render('Update Available',
+             color: :hot,
+             param1: 'update',
+             run_self: true) unless EcoBar::AutoUpdate.new.up_to_date?
+    end
+
+    def check_for_sfmono
+      font = 'SFMono-Regular.otf'
+      font_dest_path = "#{File.expand_path '~'}/Library/Fonts"
+      font_dest = "#{font_dest_path}/#{font}"
+      font_src = [
+        "/Applications/Utilities/Console.app/Contents/Resources/Fonts/#{font}",
+        "/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/#{font}"
+      ].select { |src| File.readable? src }.first
+      if font_src && !File.exists?(font_dest) && File.writable?(font_dest_path)
+        FileUtils.cp(font_src, font_dest)
+      end
+      @font = 'SFMono-Regular' if File.exists? font_dest
+    end
+
+    def check(checked)
+      case @font
+      when 'SFMono-Regular'
+        checked ? "#{CHECK} " : ' ' * 2
+      else
+        checked ? "#{CHECK} " : ' ' * 4
       end
     end
 
@@ -40,22 +72,22 @@ module EcoBar
     end
 
     def fan_mode
-      puts("Fan Mode (#{Ecobee::FanMode(thermostat.desired_fan_mode)})| " +
-           "trim=false #{color(:dark)}")
+      render("Fan Mode (#{Ecobee::FanMode(thermostat.desired_fan_mode)})",
+             color: :dark)
       Ecobee::FAN_MODES.each do |mode|
         if mode == thermostat.desired_fan_mode
-          line = "--#{CHECK} #{Ecobee::FanMode(mode)}| trim=false #{color(:dark)}"
+          render("--#{check true}#{Ecobee::FanMode(mode)}", color: :dark)
         else
-          line = "--    #{Ecobee::FanMode(mode)}| trim=false #{color(:dark)} " +
-                 "#{@base_command} param1=set_fan_mode=#{mode}"
+          render("--#{check false}#{Ecobee::FanMode(mode)}",
+                 color: :dark,
+                 param1: "set_fan_mode=#{mode}",
+                 run_self: true)
         end
-        puts line
       end
     end
 
     def header
-      puts "#{thermostat.temperature}#{DEG}"
-      puts '---'
+      puts "#{thermostat.temperature}#{DEG}\n---"
     end
 
     def max_index
@@ -63,57 +95,81 @@ module EcoBar
     end
 
     def mode_menu
-      puts "Mode (#{Ecobee::Mode(thermostat.mode)})"
+      render "Mode (#{Ecobee::Mode(thermostat.mode)})"
       Ecobee::HVAC_MODES.each do |mode|
         if mode == thermostat.mode
-          line = "--#{CHECK} #{Ecobee::Mode(mode)}| trim=false #{color(:dark)}"
+          render("--#{check true}#{Ecobee::Mode(mode)}", color: :dark)
         else
-          line = "--    #{Ecobee::Mode(mode)}| trim=false #{color(:dark)} " +
-                 %Q{#{@base_command} param1="set_mode=#{mode}"}
+          render("--#{check false}#{Ecobee::Mode(mode)}",
+                 color: :dark,
+                 param1: "set_mode=#{mode}",
+                 run_self: true)
         end
-        puts line
       end
     end
 
-
     def name_menu
-      puts "#{thermostat.name} (#{thermostat.model}) | #{color(:dark)}"
+      render("#{thermostat.name} (#{thermostat.model})", color: :dark)
       if max_index > 0
         @thermostats.each_index do |index|
           thermostat = @thermostats[index]
           
           if @index == index
-            line = "--#{CHECK} #{thermostat.name} (#{thermostat.model})| " +
-                   "trim=false #{color(:dark)}"
+            render("--#{check true}#{thermostat.name} (#{thermostat.model})",
+                   color: :dark)
           else
-            line = "--    #{thermostat.name} (#{thermostat.model})| trim=false" +
-                   " #{color(:dark)} #{@base_command} param1=set_index=#{index}"
+            render("--#{check false}#{thermostat.name} (#{thermostat.model})",
+                   color: :dark,
+                   param1: "set_index=#{index}",
+                   run_self: true)
           end
-          puts line
         end
       end
+    end
+
+    def render(msg, *args)
+      msg += '|'
+      if (arg = args.shift).is_a? Hash
+        msg += " #{arg[:attrib]}" if arg.key? :attrib
+        msg += " bash=\"#{arg[:bash]}\"" if arg.key? :bash
+        msg += " #{color(arg[:color])}" if arg.key? :color
+        if arg.key? :font
+          msg += " font=#{arg[:font]}"
+        elsif @font
+          msg += " font=#{@font}"
+        end
+        msg += " href=\"#{arg[:href]}\"" if arg.key? :href
+        msg += " #{@base_command}" if arg.key? :run_self
+        msg += " trim=#{arg[:trim] ? 'true' : 'false'}"
+        arg.keys.sort.select do |key| 
+          if key.to_s =~ /^param(\d+)/
+            msg += " param#{$1}=#{arg[key]}"
+          end
+        end
+      end
+      puts msg
     end
 
     def sensors
       puts 'Sensors'
       thermostat[:remoteSensors].sort { |a,b| a[:name] <=> b[:name] }
         .each do |sensor|
-          line = "--#{sensor[:name]}:"
+          msg = "--#{sensor[:name]}:"
           val = sensor[:capability].select { |cap| cap[:type] == 'temperature' }
-          line += %Q{ #{thermostat.unitize(val[0][:value])}#{DEG}} if val.length > 0
+          msg += %Q{ #{thermostat.unitize(val[0][:value])}#{DEG}} if val.length > 0
           val = sensor[:capability].select { |cap| cap[:type] == 'humidity' }
-          line += %Q{ #{val[0][:value].to_i}%} if val.length > 0
-          puts line + "| #{color(:dark)}"
+          msg += %Q{ #{val[0][:value].to_i}%} if val.length > 0
+          render(msg, color: :dark)
         end
     end
 
     def setpoint_menu
       if ['auto', 'cool'].include? thermostat.mode
-        puts "Cool Setpoint: #{thermostat.desired_cool}#{DEG} | #{color(:cold)}"
+        render("Cool Setpoint: #{thermostat.desired_cool}#{DEG}", color: :cold)
         setpoint_menu_cool
       end
       if ['auto', 'auxHeatOnly', 'heat'].include? thermostat.mode
-        puts "Heat Setpoint: #{thermostat.desired_heat}#{DEG} | #{color(:hot)}"
+        render("Heat Setpoint: #{thermostat.desired_heat}#{DEG}", color: :hot)
         setpoint_menu_heat
       end
     end
@@ -121,24 +177,26 @@ module EcoBar
     def setpoint_menu_cool
       thermostat.cool_range(with_delta: true).reverse_each do |temp|
         if temp == thermostat.desired_cool
-          line = "--#{CHECK} #{temp}#{DEG}| trim=false #{color(:cold)}"
+          render("--#{check true}#{temp}#{DEG}", color: :cold)
         else
-          line = "--    #{temp}#{DEG}| trim=false #{color(:cold)} " +
-                 "#{@base_command} param1=set_cool=#{temp}"
+          render("--#{check false}#{temp}#{DEG}",
+                 color: :cold,
+                 param1: "set_cool=#{temp}",
+                 run_self: true)
         end
-        puts line
       end
     end
 
     def setpoint_menu_heat
       thermostat.heat_range(with_delta: true).reverse_each do |temp|
         if temp == thermostat.desired_heat
-          line = "--#{CHECK} #{temp}#{DEG}| trim=false #{color(:hot)}"
+          render("--#{check true}#{temp}#{DEG}", color: :hot)
         else
-          line = "--    #{temp}#{DEG}| trim=false #{color(:hot)} " +
-                 "#{@base_command} param1=set_heat=#{temp}"
+          render("--#{check false}#{temp}#{DEG}", 
+                 color: :hot,
+                 param1: "set_heat=#{temp}",
+                 run_self: true)
         end
-        puts line
       end
     end
 
@@ -147,7 +205,7 @@ module EcoBar
     end
 
     def status
-      puts "Status (#{thermostat[:equipmentStatus]}) | #{color(:dark)}"
+      render("Status (#{thermostat[:equipmentStatus]})", color: :dark)
     end
 
     def thermostat
@@ -155,7 +213,7 @@ module EcoBar
     end
 
     def website
-      puts 'Ecobee Web Portal|href="https://www.ecobee.com/consumerportal/index.html"'
+      render('Ecobee Web Portal', href: ECOBEE_URL)
     end
 
     private
